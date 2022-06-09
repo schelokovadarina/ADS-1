5c2f88c12d34fec83043617fadfca969eec9b078
schelokovadarina <99324863+schelokovadarina@users.noreply.github.com>
2022-06-09 11:10:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,13 @@
 #include <cmath>
 bool checkPrime(uint64_t value) {
 if (value > 1) {
-for (int i = 2; i <= (sqrt(abs(value))); i++)
-if (value % i == 0) {
-return false;
-}
-return true;
+	for (int i = 2; i <= (sqrt(abs(value))); i++)
+		if (value % i == 0) {
+			return false;
+		}
+	return true;
 } else {
-return false;
+	return false;
 }
 }
 
@@ -18,9 +18,9 @@ uint64_t nPrime(uint64_t n) {
 uint64_t num = 0;
 uint64_t i = 0;
 while (i != n) {
-num++;
-if (checkPrime(num))
-i++;
+	num++;
+	if (checkPrime(num))
+		i++;
 }
 return num;
 }
@@ -29,7 +29,7 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t nextnum = 0;
 nextnum = nextnum + value + 1;
 while (!(checkPrime(nextnum))) {
-nextnum++;
+	nextnum++;
 }
 return nextnum;
 }
@@ -37,10 +37,10 @@ return nextnum;
 uint64_t sumPrime(uint64_t hbound) {
 uint64_t sum = 0;
 if (hbound > 2) {
-for (int i = 2; i < hbound; i++)
-if (checkPrime(i)) {
-sum = sum + i;
-}
+	for (int i = 2; i < hbound; i++)
+		if (checkPrime(i)) {
+			sum = sum + i;
+		}
 }
 return sum;
 }
